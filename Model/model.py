c739d6eae545a4e35862e93f24d487d117ccdf42
Artur Kuramshin <artur.kurams@gmail.com>
2021-05-14 10:09:55 -0400

    Finished training loop

--- Model/model.py
+++ Model/model.py
@@ -163,4 +163,72 @@ netD.apply(weights_init)
 
 criterion = nn.BCELoss()
 
-fixed_noise = torch.randn()
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+real_label = 1.
+fake_label = 0.
+
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+
+# Lists to keep track of progress
+img_list = []
+G_losses = []
+D_losses = []
+iters = 0
+
+print("Starting training loop...")
+for epoch in range(num_epochs):
+    for i, data in enumerate(dataloader, 0):
+        # Update D network: maximize log(D(x)) + log(1 - D(G(z)))
+        # 1.) Train with real images
+        netD.zero_grad()
+        real_cpu = data[0].to(device)
+        cat = data[1].to(device)
+        b_size = real_cpu.size(0)
+        label = torch.full((b_size,), real_label, dtype=torch.float, device=device)
+
+        output = netD(real_cpu, cat).view(-1)
+        errD_real = criterion(output, label)
+        errD_real.backward()
+        D_x = output.mean().item()
+
+        # 2.) Train with fake
+        noise = torch.randn(b_size, nz, 1, 1, device=device)
+        fake = netG(noise, cat)
+        label.fill_(fake_label)
+
+        output = netD(fake.detach()).view(-1)
+        errD_fake = criterion(output, label)
+        errD_fake.backward()
+        D_G_z1 = output.mean().item()
+        errD = errD_real + errD_fake
+        # Update D
+        optimizerD.step()
+
+        # Update G network: maximize log(D(G(z)))
+        netG.zero_grad()
+        label.fill_(real_label)
+
+        output = netD(fake).view(-1)
+        errG = criterion(output, label)
+        errG.backward()
+        D_G_z2 = output.mean().item()
+        optimizerG.step()
+
+        # Output training stats
+        if i % 50 == 0:
+            print('[%d/%d][%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f\tD(x): %.4f\tD(G(z)): %.4f / %.4f'
+                  % (epoch, num_epochs, i, len(dataloader),
+                     errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
+
+        # Save Losses for plotting later
+        G_losses.append(errG.item())
+        D_losses.append(errD.item())
+
+        # Check how the generator is doing by saving G's output on fixed_noise
+        if (iters % 500 == 0) or ((epoch == num_epochs-1) and (i == len(dataloader)-1)):
+            with torch.no_grad():
+                fake = netG(fixed_noise).detach().cpu()
+            img_list.append(vutils.make_grid(fake, padding=2, normalize=True))
+
+        iters += 1
