b9495dfea90dc5c58aaa6e6269f6da7aa0b8611c
Artur Kuramshin <artur.kurams@gmail.com>
2021-05-20 14:22:19 -0400

    Added dimension calc for both conv2d and conv2dtranspose

--- dimensionscalc.py
+++ dimensionscalc.py
@@ -1,11 +1,15 @@
-stride = 2
-padding = 1
+stride = 1
+padding = 0
 kernel_size = 4
 dilation = 1
 output_padding = 0
-H_in = 7
+H_in =4
 
 
+# ConvTranspose
+#H_out = (H_in-1)*stride - 2*padding + dilation*(kernel_size-1) + output_padding + 1
 
-H_out = (H_in-1)*stride - 2*padding + dilation*(kernel_size-1) + output_padding + 1
+#Conv2D
+H_out = ((H_in + 2*padding - dilation*(kernel_size-1)-1)/stride)+1
+
 print(H_out)
